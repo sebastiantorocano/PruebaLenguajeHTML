7133a53320f484d45c530d8b966e6b629a1d7279
Lenovo\Usuario <sebastian_toro24131@elpoli.edu.co>
2017-11-25 09:33:09 -0500

    prueba

---
+++ src/main/java/com/example/PruebaLenguajeHTML/PruebaLenguajeHtmlApplication.java
@@ -0,0 +1,12 @@
+package com.example.PruebaLenguajeHTML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaLenguajeHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaLenguajeHtmlApplication.class, args);
+	}
+}
