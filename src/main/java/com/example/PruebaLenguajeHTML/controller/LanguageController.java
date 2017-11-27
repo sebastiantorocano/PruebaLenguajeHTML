509504961085ac78a74fe0645a13bd962116d1b6
Lenovo\Usuario <sebastian_toro24131@elpoli.edu.co>
2017-11-26 22:41:12 -0500

    solution of technical test

--- src/main/java/com/example/PruebaLenguajeHTML/controller/LanguageController.java
+++ src/main/java/com/example/PruebaLenguajeHTML/controller/LanguageController.java
@@ -45,31 +45,49 @@ public class LanguageController {
 	@PostMapping("/languageSimbol")
 	public String getLanguaje(@RequestParam(value="word") String word) {
 		
-		List<LanguageModel> language=languageRepository.findAll();
-		String completeHtml="";
-		
 		Utils util= new Utils();
+		List<LanguageModel> language=languageRepository.findAll();
+		boolean flag=util.countSpaces(word,language);
+		
+		String completeHtml="";
+		 completeHtml = "<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n" + "<meta charset=\"utf-8\">\r\n"
+
+				+ "<title></title>\r\n" + "</head>\r\n" + "<body>\r\n";
+		
+		
+		if(flag==true) {
+			completeHtml=completeHtml+word+"\r\n";
+		}else {
+		
+		
+		
+		
+		
+		
 		List<String> wordsList=util.distributeWords(word, language);
 		
 		List<String> labelHtmlArray=new  ArrayList<String>();
 		
 		for(int j=0;j<wordsList.size();j++) {
 			String wordProccess=wordsList.get(j).replace("-", " ");
+			
+		
 			wordProccess = wordProccess.trim();
 			String[] arrayWord = wordProccess.split(" ");
 			String simb = arrayWord[0].charAt(0) + "";
+			
+			System.out.println("word process "+wordProccess);
 			
 		String labelHtml=	util.validationSimbol(languageRepository.findBySimbol(simb), arrayWord);
 		labelHtmlArray.add(labelHtml);
 		}
 		
-		 completeHtml = "<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n" + "<meta charset=\"utf-8\">\r\n"
-
-				+ "<title></title>\r\n" + "</head>\r\n" + "<body>\r\n";
+		
 		 
 		for(int i=0;i<labelHtmlArray.size();i++) {
 			completeHtml=completeHtml+labelHtmlArray.get(i)+"\r\n";
 		}
+	}
 		
 		
 		completeHtml = completeHtml + "</body>\r\n" + "</html>";
--- src/main/java/com/example/PruebaLenguajeHTML/utils/Utils.java
+++ src/main/java/com/example/PruebaLenguajeHTML/utils/Utils.java
@@ -9,9 +9,15 @@ public class Utils {
 
 	public String validationSimbol(List<LanguageModel> list, String[] arrayWord) {
 
+		String word ="";
+		
 		String content = list.get(0).getContent();
 		String simbol = list.get(0).getSimbol();
-		String word = arrayWord[1];
+		
+		for(int i=1;i<arrayWord.length;i++) {
+			 word = word+arrayWord[i]+" ";
+		}
+		
 		int count = arrayWord[0].length();
 
 		String completeHtml = "";
@@ -26,40 +32,74 @@ public class Utils {
 		return completeHtml;
 	}
 
-	public  List<String>  distributeWords(String words,List<LanguageModel> language) {
-		 List<String> wordsList = new 	ArrayList<String>();
-		
-		String simbol = "";
-		String wordComplete="";
-int countEmpty=0;
+	public boolean countSpaces(String words, List<LanguageModel> language) {
+		int spaces = 0;
+		int countSimbol=0;
+		boolean flag=false;
 		for (int i = 0; i < words.length(); i++) {
-			String c = words.charAt(i)+"";
-			
+			String c = words.charAt(i) + "";
 
-			for(int j=0;j<language.size();j++) {
-				if(c.equals(language.get(j).getSimbol().trim())) {
-					System.out.println("simbolos "+language.get(j).getSimbol().trim());
-					
-					simbol=simbol+c;
-					
-				}else {
-					
-					if(c.equals(" ")) {
+			for (int j = 0; j < language.size(); j++) {
+				if (c.equals(language.get(j).getSimbol().trim())) {
+					countSimbol++;
+				}
+				
+				if(c.equals(" ") && countSimbol>0) {
+				spaces++;
+				}
+				
+				
+				
+			}
+		}
+		if(countSimbol>0 && spaces==0) {
+			flag=true;
+		}
+		
+		
+
+		return flag;
+	}
+
+	public List<String> distributeWords(String words, List<LanguageModel> language) {
+		List<String> wordsList = new ArrayList<String>();
+
+		String simbol = "";
+		String wordComplete = "";
+		int countEmpty = 0;
+		boolean flag = false;
+		for (int i = 0; i < words.length(); i++) {
+			String c = words.charAt(i) + "";
+			for (int j = 0; j < language.size(); j++) {
+				if (c.equals(language.get(j).getSimbol().trim())) {
+					System.out.println("simbolos " + language.get(j).getSimbol().trim());
+
+					simbol = simbol + c;
+					flag = true;
+
+				} else {
+
+					if (c.equals(" ")) {
+						
 						countEmpty++;
-						wordComplete=wordComplete+"-";
-					}else {
-						wordComplete=wordComplete+c;
+						wordComplete = wordComplete + "-";
+						
+						System.out.println("vacio "+wordComplete+" cont "+countEmpty);
+					} else {
+						wordComplete = wordComplete + c;
 					}
-					
+
 				}
 			}
-			if(countEmpty>2) {
-				wordsList.add(simbol+wordComplete);
-				countEmpty=0;
-				wordComplete="";
-				simbol="";
+
+			if (countEmpty > 2) {
+				wordsList.add(simbol + wordComplete);
+				System.out.println("añadir  "+simbol + wordComplete);
+				countEmpty = 0;
+				wordComplete = "";
+				simbol = "";
 			}
-			
+
 		}
 		return wordsList;
 
